51054e450782895c859869780e946d996a0a07e9
Hygor Melo <hygormelo96@gmail.com>
2023-07-02 12:32:46 -0300

    feat: create user dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
-  @Get()
-  async getAllUsers() {
-    return JSON.stringify({ test: 'abc' });
+  @Post()
+  async createUser(@Body() createUser: CreateUserDTO) {
+    return {
+      ...createUser,
+      password: undefined,
+    };
   }
 }
